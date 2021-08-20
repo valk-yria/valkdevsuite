187164d858f08042e77f4d5e699d457e5905581c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-20 16:30:10 -0500

    :art: Format with Stylua

--- Scripts/OLD.server.lua
+++ Scripts/OLD.server.lua
@@ -2,22 +2,20 @@
 -- This was before I did a code overhaul because the entire script was getting too long and illegible.
 -- I intend to copy and paste some of the code I've wrote here to the other scripts.
 
-
-
 ---- declare functions
 --local function SelectByProperties()
 --	local t = {}
 --	local p = {}
-	
+
 --	for _, v in pairs(workspace:GetDescendants()) do
 --		if (textBoxes[1].Text ~= "") then
 --			table.insert(p, #p, v)
 --		end
 --	end
-	
+
 --	SelectionService:Set(t)
 --	warn("Selected parts in workspace by properties.")
-	
+
 --	for i = 1, 3, 1 do
 --		textBoxes[i].Text = ""
 --	end
@@ -26,8 +24,8 @@
 --local function GetCurrentTransformButton()
 --	local cf = cam.CFrame
 --	local x, y, z = cf:ToEulerAnglesXYZ()
---	--textBoxes[4].Text = 
---	--textBoxes[5].Text = 
+--	--textBoxes[4].Text =
+--	--textBoxes[5].Text =
 --end
 
 --local WrappedScanMapButton = pluginv.DisableButton(ScanMapButton, textButtons[2], disabledColor3.Value, "Cooldown")
--- Scripts/README.server.lua
+++ Scripts/README.server.lua
@@ -8,4 +8,5 @@ VERSION: 1.3
 Refrain from modifying the code here unless you really know what you are doing.
 If you're really enthusiastic on knowing how it works though, fire me a DM on Discord! @valkyria#0001
 
-]]--
+]]
+--
